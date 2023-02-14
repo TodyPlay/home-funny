41afaa13451591b1164c8e27b1765aef63c5cdf5
jianchen <837795138@qq.com>
2023-02-14 23:31:02 +0800

    # Specification查询<待测试>

--- home-funny-application/src/main/java/com/home/funny/repository/HomeFunnyMultiMediaRepository.java
+++ home-funny-application/src/main/java/com/home/funny/repository/HomeFunnyMultiMediaRepository.java
@@ -1,24 +1,11 @@
 package com.home.funny.repository;
 
-import com.home.funny.dto.media.MediaQueryDTO;
 import com.home.funny.model.HomeFunnyMultiMedia;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HomeFunnyMultiMediaRepository extends JpaRepository<HomeFunnyMultiMedia, Long> {
+public interface HomeFunnyMultiMediaRepository extends JpaRepository<HomeFunnyMultiMedia, Long>, JpaSpecificationExecutor<HomeFunnyMultiMedia> {
 
-    @Query("""
-            select distinct media from HomeFunnyMultiMedia media
-            left join HomeFunnyMediaTagMapping mapping on mapping.multiMedia.id = media.id
-            left join HomeFunnyMediaTag tag on tag.id = mapping.mediaTag.id
-            where media.name like %:#{#query.name}%
-            and media.mediaType in :#{#query.type}
-            and tag.name in :#{#query.tags}
-            and media.createDate between :#{#query.date[0]} and :#{#query.date[1]}
-            """)
-    Page<HomeFunnyMultiMedia> findAll(MediaQueryDTO query, Pageable pageable);
 }
--- home-funny-application/src/main/java/com/home/funny/service/MediaService.java
+++ home-funny-application/src/main/java/com/home/funny/service/MediaService.java
@@ -8,8 +8,13 @@ import com.home.funny.repository.HomeFunnyMediaTagRepository;
 import com.home.funny.repository.HomeFunnyMultiMediaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,8 +29,22 @@ public class MediaService {
     }
 
     public Page<HomeFunnyMultiMedia> findMedias(MediaQueryDTO query, PageableDTO page) {
+        Specification<HomeFunnyMultiMedia> sf = (root, cq, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
 
-        return homeFunnyMultiMediaRepository.findAll(query,page.getPageRequest());
+            if (StringUtils.hasText(query.getName())) {
+                Predicate nameP = cb.like(root.get("name").as(String.class), query.getName());
+                predicates.add(nameP);
+            }
+
+            Predicate and = cb.and(predicates.toArray(new Predicate[0]));
+
+            cq.where(and);
+
+            return cq.getRestriction();
+        };
+
+        return homeFunnyMultiMediaRepository.findAll(sf, page.getPageRequest());
     }
 
 }
