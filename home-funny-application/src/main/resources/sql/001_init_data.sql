3d52be373a3b9e5ba325e410cedb5a869306801e
jianchency <jianchency@isoftstone.com>
2023-02-17 16:51:18 +0800

    # 更新脚本

--- home-funny-application/src/main/resources/sql/001_init_data.sql
+++ home-funny-application/src/main/resources/sql/001_init_data.sql
@@ -1,18 +1,22 @@
 insert into home_funny_media_tag (id, name) value (1, '血腥'), (2, '暴力'), (3, '文艺'), (4, '青春');
 
+insert into home_funny_file (id, file_group, file_name, file_path) VALUE
+    (1, 'video', '计划.zip', '计划.zip'),
+    (2, 'video', '计划2.zip', '计划2.zip');
+
 insert into home_funny_multi_media
-    (name, cover, media_type, description, create_date) VALUE
-    ('宝贝计划1', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划2', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划3', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划4', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划5', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划6', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划7', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划8', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划9', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划10', '封面', 'VIDEO', '描述', '2022-12-17'),
-    ('宝贝计划11', '封面', 'VIDEO', '描述', '2022-12-17');
+    (id, name, cover, media_type, description, cover_file_id, create_date) VALUE
+    (1, '宝贝计划1', '封面', 'VIDEO', '描述', 1, curdate()),
+    (2, '宝贝计划2', '封面', 'VIDEO', '描述', 1, curdate()),
+    (3, '宝贝计划3', '封面', 'VIDEO', '描述', 1, curdate()),
+    (4, '宝贝计划4', '封面', 'VIDEO', '描述', 1, curdate()),
+    (5, '宝贝计划5', '封面', 'VIDEO', '描述', 1, curdate()),
+    (6, '宝贝计划6', '封面', 'VIDEO', '描述', 1, curdate()),
+    (7, '宝贝计划7', '封面', 'VIDEO', '描述', 1, curdate()),
+    (8, '宝贝计划8', '封面', 'VIDEO', '描述', 1, curdate()),
+    (9, '宝贝计划9', '封面', 'VIDEO', '描述', 1, curdate()),
+    (10, '宝贝计划10', '封面', 'VIDEO', '描述', 1, curdate()),
+    (11, '宝贝计划11', '封面', 'VIDEO', '描述', 1, curdate());
 
 insert into home_funny_media_tag_mapping
     (media_id, tag_id) VALUE
@@ -31,7 +35,3 @@ insert into home_funny_media_tag_mapping
     (5, 1),
     (5, 2),
     (5, 3);
-
-insert into home_funny_file (file_group, file_name, file_path) VALUE
-    ('video', '计划.zip', '计划.zip'),
-    ('video', '计划2.zip', '计划2.zip')
