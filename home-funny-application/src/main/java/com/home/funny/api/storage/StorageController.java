d07207226b71279a00cb13cc72f748166f7838ad
jianchency <jianchency@isoftstone.com>
2023-02-22 17:24:22 +0800

    # 完善

--- home-funny-application/src/main/java/com/home/funny/api/storage/StorageController.java
+++ home-funny-application/src/main/java/com/home/funny/api/storage/StorageController.java
@@ -6,20 +6,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/api/v1/storage")
 public class StorageController {
 
     @Autowired
     private StorageService storageService;
-
-    @GetMapping("/video/{id}")
-    public void video(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        storageService.video(id, request, response);
-    }
 
     @GetMapping("download/{id}")
     public ResponseEntity<Resource> download(@PathVariable Long id, @RequestHeader(value = "Range", required = false) String range) throws Exception {
--- home-funny-application/src/main/java/com/home/funny/service/StorageService.java
+++ home-funny-application/src/main/java/com/home/funny/service/StorageService.java
@@ -6,17 +6,12 @@ import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.StatObjectArgs;
 import io.minio.StatObjectResponse;
-import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -46,16 +41,16 @@ public class StorageService {
         HttpStatus status;
 
         if (range != null && range.startsWith("bytes=")) {
-            String range_ = range.replaceAll("bytes=", "");
+            String _range = range.replaceAll("bytes=", "");
 
-            if (range_.startsWith("-")) {
+            if (_range.startsWith("-")) {
                 rangeStart = 0;
-                rangeEnd = Long.parseLong(range_.split("-")[0]);
-            } else if (range_.endsWith("-")) {
-                rangeStart = Long.parseLong(range_.split("-")[0]);
+                rangeEnd = Long.parseLong(_range.split("-")[0]);
+            } else if (_range.endsWith("-")) {
+                rangeStart = Long.parseLong(_range.split("-")[0]);
                 rangeEnd = stat.size() - 1;
             } else {
-                String[] range_2 = range_.split("-");
+                String[] range_2 = _range.split("-");
                 rangeStart = Long.parseLong(range_2[0]);
                 rangeEnd = Long.parseLong(range_2[1]);
             }
@@ -66,7 +61,7 @@ public class StorageService {
             rangeStart = 0;
             rangeEnd = stat.size() - 1;
             status = HttpStatus.OK;
-            length = stat.size() - 1;
+            length = rangeEnd - rangeStart;
         }
 
         httpHeaders.setContentDisposition(ContentDisposition.attachment().filename(URLEncoder.encode(storage.getStorageName(), StandardCharsets.UTF_8)).build());
@@ -83,73 +78,4 @@ public class StorageService {
         return new ResponseEntity<>(new InputStreamResource(stream), httpHeaders, status);
     }
 
-    public void videoPreview(String bucketName, String path, HttpServletRequest request, HttpServletResponse res) throws Exception {
-
-        StatObjectResponse statObjectResponse = minioClient.statObject(StatObjectArgs.builder().bucket(bucketName).object(path).build());
-        long fileSize = statObjectResponse.size();
-
-        final long bufferStep = 1024 * 1024 * 3; // 流区间
-        long startPos = 0; // 流起始位置
-        long endPos = fileSize - 1; // 流结束位置
-
-        String rangeHeader = request.getHeader("Range");
-        if (!ObjectUtils.isEmpty(rangeHeader) && rangeHeader.startsWith("bytes=")) {
-            try {
-                // 断点续传 状态码206
-                res.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-
-                // 情景一：RANGE: bytes=2000070- 情景二：RANGE: bytes=2000070-2000970
-                String numRang = request.getHeader("Range").replaceAll("bytes=", "");
-                if (numRang.startsWith("-")) {
-                    startPos = 0;
-                    endPos = endPos - Long.parseLong(new String(numRang.getBytes(StandardCharsets.UTF_8), 1, numRang.length() - 1));
-                } else if (numRang.endsWith("-")) {
-                    startPos = Long.parseLong(new String(numRang.getBytes(StandardCharsets.UTF_8), 0, numRang.length() - 1));
-                    endPos = fileSize - 1;
-                } else {
-                    String[] strRange = numRang.split("-");
-                    if (strRange.length == 2) {
-                        startPos = Long.parseLong(strRange[0].trim());
-                        endPos = Long.parseLong(strRange[1].trim());
-                    }
-                }
-
-                if (startPos < 0 || endPos < 0 || endPos >= fileSize || startPos > endPos) {
-                    // range不满足
-                    res.setStatus(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE);
-                    return;
-                }
-
-            } catch (NumberFormatException e) {
-                startPos = 0;
-            }
-        }
-
-
-        if (endPos - startPos + 1 > bufferStep) {
-            endPos = startPos + bufferStep - 1; //设置为定长流区间
-        }
-        if (endPos >= fileSize) {
-            endPos = fileSize - 1;
-        }
-        long resContentLength = bufferStep; // response内容长度
-        if (endPos == fileSize - 1) {
-            resContentLength = endPos - startPos + 1; // 设置为文件剩余的实际长度
-        }
-
-        res.setHeader("Accept-Ranges", "bytes");
-        res.setHeader("Content-Range", String.format("bytes %d-%d/%d", startPos, endPos, fileSize));
-        res.addHeader("Content-Length", String.valueOf(resContentLength));
-
-        GetObjectArgs objectArgs2 = GetObjectArgs.builder().bucket(bucketName).object(path).offset(startPos).length(bufferStep).build();
-
-        try (InputStream tmpStream = minioClient.getObject(objectArgs2); ServletOutputStream stream = res.getOutputStream()) {
-            IOUtils.copy(tmpStream, stream);
-        }
-    }
-
-    public void video(Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        HomeFunnyStorage storage = storageRepository.findById(id).orElseThrow();
-        this.videoPreview(storage.getStorageGroup(), storage.getStoragePath(), request, response);
-    }
 }
